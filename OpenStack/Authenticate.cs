0af92261e6c8290347835b0067844e80b7f8b3ff
Riju Vashisht <rijuvashisht@gmail.com>
2013-03-26 13:19:38 -0400

    First instance
    Men AT work

---
+++ OpenStack/Authenticate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenStack
+{
+    interface Authenticate
+    {
+        public virtual String Authenticate();
+        public virtual Boolean CreateFolder(String foldername);
+        public virtual Boolean DeleteFolder(String foldername);
+        public virtual void ListFolders();
+        public virtual Boolean UploadFile(String foldername);
+        public virtual Boolean DownloadFile(String filename);
+        public virtual Boolean DeleteFile(String foldername);
+        public virtual void ListFiles();
+
+
+    }
+}
---
+++ OpenStack/Provider.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenStack
+{
+    public enum Provider { Google, Windows, Rackspace, OpenStack };   
+}
